6bf4eeff8c2e857f01e52f423cdcd800dbbd7442
sahebmanna <www.mannasaheb@gmail.com>
2026-07-09 00:02:26 +0530

    mongoose pre hook middleware,mo0ngodb aggregation pipeline and  userdefinef method write

---
+++ src/models/user.model.js
@@ -0,0 +1,96 @@
+import mongoose from "mongoose";
+
+import jwt from "jsonwebtoken";
+
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema(
+  {
+    UserName: {
+      type: String,
+      required: true,
+      index: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    Avatar: {
+      type: String, //cloudenary url
+      required: true,
+    },
+    CoverImage: {
+      type: String, //cloudenary url
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password Required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+//it's a pre hook middleware bifore saving data saved in mongodb it is used for hashed
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// this function for comparing the hashed password with user given poassword
+userSchema.methods.isPassword = async function (password) {
+  return await bcript.compare(password, this.password);
+};
+
+//generate access token
+
+userSchema.methods.generateAccewssToken = async function () {
+  return await jwt.sign(
+    {
+      _id: this._id,
+      UserName: this.UserName,
+      email: this.email,
+      fullname: this.fullname,
+    },
+    ACCESS_TOKEN_SECRET,
+    {
+      expireIn: ACCES_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = async function () {
+  return await jwt.sign(
+    {
+      _id: this._id,
+    },
+    REFRESH_TOKEN_SECRET,
+    {
+      expireIn: REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
---
+++ src/models/video.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new mongoose.Schema(
+  {
+    videoFile: {
+      type: String,
+      required: [true, "video is required"],
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number, //cloudenary url
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: true,
+    },
+    isPublished: {
+      type: Bool,
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", videoSchema);
